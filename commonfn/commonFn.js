e71d85406020ed5e170fee168664cf2552513144
Levison <389169974@qq.com>
2017-12-21 18:38:51 +0800

    refresh

--- commonfn/commonFn.js
+++ commonfn/commonFn.js
@@ -2,11 +2,13 @@ import fetch from "node-fetch";
 import formidable from "formidable";
 import path from "path";
 import fs from "fs";
+// 对图片剪切缩放
 import gm from "gm";
+import uuid from "uuid";
 
 export default class CommonFn {
     constructor() {
-        this.uploadImg = this.uploadImg.bind(this)
+        this.uploadImg = this.uploadImg.bind(this);
     }
     async uploadImg(req, res, next) {
         let type = res.params.type;
@@ -23,6 +25,35 @@ export default class CommonFn {
                 message: "上传图片失败"
             })
         }
+    }
+
+    async getPath(){
+        return new Promise((resolve, reject) => {
+            //创建一个新的incomingform表单
+            const form = formidable.IncomingForm();
+            form.uploadDir = "./public/images";
+            form.parse(req, async (err, fields, files) => {
+                let img_id = uuid.v1();
+                let fName = img_id + path.extname(files.file.name);
+                let path = "./public/images" + fName;
+                try{
+                    await fs.rename(files.file.path, path);
+                    gm(path)
+                    .resize(200, 200, "!")
+                    .write(path, async (err) => {
+                        if(err){
+                            reject("裁剪失败");
+                            return
+                        }
+                        resolve(fName);
+                    })
+                }catch(err){
+                    console.log("err:",err);
+                    fs.unlink(files.file.path);
+                    reject("保存图片失败");
+                }
+            })
+        })
     }
 
 }
--- commonfn/location.js
+++ commonfn/location.js
@@ -1,1 +1,10 @@
-import commonfn from './commonFn'
+import CommonFn from './commonFn'
+
+class Location extends CommonFn {
+    constructor(){
+        super();
+        this.baiduak = "Ch5SXX8WxKMsN5DGddOjGIh7v5vlG6zK";
+
+    }
+
+}
