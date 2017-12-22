11f94e6f517261aa0abeaa67b4c949cca8b87321
Levison <389169974@qq.com>
2017-12-22 18:53:56 +0800

    refresh

--- commonfn/location.js
+++ commonfn/location.js
@@ -4,7 +4,30 @@ class Location extends CommonFn {
     constructor(){
         super();
         this.baiduak = "Ch5SXX8WxKMsN5DGddOjGIh7v5vlG6zK";
-
+    }
+    async getPosition(req){
+        return new Promise(async (resolve, reject) => {
+            let ip = req.header["x-forwarded-for"];
+            let ipList = ip.split(":");
+            ip = ipList.pop();
+            if(process.env.NODE_ENV == "development"){
+                ip = "180.173.33.7"
+            }
+            try{
+                let result;
+                result = await this.fetch("https://api.map.baidu.com/location/ip", {
+                    ip,
+                    AK: this.baiduak
+                })
+                if(result.status == 0){
+                    let cityInfo = {
+                        city: result.content.address_detail.city,
+                    }
+                }
+            }catch(err){
+                reject("定位失败")
+            }
+        })
     }
 
 }
