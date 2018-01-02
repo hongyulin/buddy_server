1601cc3927423812c5868d48d075656ef18e6966
Levinson <389169974@qq.com>
2018-01-03 00:22:16 +0800

    refresh

--- spider/userInfo.js
+++ spider/userInfo.js
@@ -1,8 +1,40 @@
+import uuid from "uuid"
 export default [
     {
-        name: "linhongyu"
+        id: uuid.v4(),
+        header_img: "",
+        name: "林红雨"
+        sex: "man",
+        age: 26,
+        level: 1,
+        step_Num: 1101,
+        following: [
+            {
+                id: uunid.v4(),
+                name: "",
+                header_img: "default_header.jpg"
+            },
+        ],
+        login_time: 2016-12-26,
+        self_img: [
+            ""
+        ],
+        exercise_time: 45,
+        fans: 56,
+        choice_num: 2,
+        longitude: "",
+        latitude: "",
+        circle: [
+            {
+                name: "",
+                header_img: "",
+                id: uuid.v4(),
+            }
+        ]
+        
     },
     {
+        id: uuid.v4(),
         name: "levinson"
     }
 ]
