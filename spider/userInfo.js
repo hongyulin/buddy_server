656a864beee29c8e3a158debfc6d5c6f477281ac
Levison <389169974@qq.com>
2018-01-03 19:13:52 +0800

    初始化的数据

--- spider/userInfo.js
+++ spider/userInfo.js
@@ -1,40 +1,828 @@
 import uuid from "uuid"
 export default [
-    {
-        id: uuid.v4(),
-        header_img: "",
-        name: "林红雨"
-        sex: "man",
-        age: 26,
-        level: 1,
-        step_Num: 1101,
-        following: [
-            {
-                id: uunid.v4(),
-                name: "",
-                header_img: "default_header.jpg"
-            },
-        ],
-        login_time: 2016-12-26,
-        self_img: [
-            ""
-        ],
-        exercise_time: 45,
-        fans: 56,
-        choice_num: 2,
-        longitude: "",
-        latitude: "",
-        circle: [
-            {
-                name: "",
-                header_img: "",
-                id: uuid.v4(),
-            }
-        ]
-        
-    },
-    {
-        id: uuid.v4(),
-        name: "levinson"
-    }
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "levinson",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "养熊阿姨",
+    sex: "man",
+    age: 18,
+    level: 5,
+    step_Num: 52453,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 800,
+    fans: 5,
+    choice_num: 4,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "肌不可失i",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "nice華仔",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "心微凉丶黑米",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "梦梦梦魇来袭",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "__真讽刺z",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "丶小狐狸_",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "莎哟哟YO",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "结局偏不有趣",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "惠伯",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "嘴55",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "原子核不寂寞",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "花落绪",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "qq2539845629",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "熱月",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "愤怒的小土堆",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "星火纪元",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "骑士剪刀手",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "云之安逸啊",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "随便打桶酱油",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "精神小伙05",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "夜行小正太",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "小佐丫",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
+{
+    id: uuid.v4(),
+    header_img: "",
+    name: "lei12gui",
+    sex: "man",
+    age: 26,
+    level: 1,
+    step_Num: 1101,
+    following: [
+        {
+            id: uunid.v4(),
+            name: "",
+            header_img: ""
+        },
+    ],
+    login_time: (new Date()).toISOString(),
+    self_img: [
+        ""
+    ],
+    exercise_time: 45,
+    fans: 56,
+    choice_num: 2,
+    longitude: "",
+    latitude: "",
+    circle: [
+        {
+            name: "",
+            header_img: "",
+            id: uuid.v4(),
+        }
+    ]
+    
+},
 ]
